a0554cb7bdaa5898a9b6b662b5fa7baacd76fe6c
mac <mac@macdeMBP.lan>
2022-11-10 13:25:47 +0800

    feat

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import Pinia from '@/store'
 import App from './App.vue'
 
 import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 import GlobalComponents from '@/components'
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // 如果编辑器提示 path 模块找不到，则可以安装一下 @types/node -> npm i @types/node -D
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src') // 设置 `@` 指向 `src` 目录
