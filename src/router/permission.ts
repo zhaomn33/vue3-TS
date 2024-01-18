93d338eded56b2387b6361f1e598db74717fd81e
zhao33 <102125344+zhaomn33@users.noreply.github.com>
2024-01-18 15:37:29 +0800

    fix: modify token and login (#5)

--- src/router/permission.ts
+++ src/router/permission.ts
@@ -8,23 +8,21 @@ import 'nprogress/nprogress.css'
 router.beforeEach(async(to, from, next) => {
   NProgress.start() // 开启顶部加载动画
   const userAccountStore = useUserAccount()
-  // console.log('beforeEach to' ,to);
-  console.log("Cookie.get('token')", Cookie.get('token'))
+  // console.log(to.name, 'router')
+  // console.log(Cookie.get('token'), 'token')
 
-  // 如果不存在 token 则直接返回登录页
-  if (!Cookie.get('token')) {
-    console.log('object')
-    next()
+  // 如果不存在 token，且接下来不是去登陆页，则直接返回登录页
+  if (!Cookie.get('token') && to.name !== 'login') {
+    next('/login')
     NProgress.done() // 关闭顶部加载动画
 
     return
   }
   // 如果存在 token 通过获取用户信息校验 token 有效性
   const { data, error } = await userAccountStore.getUserInfo()
-  console.log('data', data, error)
+  // console.log('data', data, error)
 
   if (error){
-
     Cookie.remove('token')
     next('/login')
 
